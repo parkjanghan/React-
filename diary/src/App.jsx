55893a04b966c08faf7b264227d2850066a4e604
proud_oooof <parkjanghan2256@gmail.com>
2025-08-03 23:43:33 +0900

    feat : Pages > Edit 페이지 추가

--- diary/src/App.jsx
+++ diary/src/App.jsx
@@ -1,38 +1,41 @@
 import './App.css'
-import { Routes, Route, Link, useNavigate } from 'react-router-dom'
+import { useReducer } from 'react'
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/home'
 import New from './pages/New'
 import Diary from './pages/Diary'
+import Edit from './pages/Edit'
 import Notfound from './pages/Notfound'
 
-import { getEmotionImage } from './util/get-emotion-image'
+const mockData = [
+  {
+    id: 1,
+    createdDate: new Date().getTime(),
+    emotionId: 1,
+    content: '1번 일기 내용',
+  },
+  {
+    id: 2,
+    createdDate: new Date().getTime(),
+    emotionId: 2,
+    content: '2번 일기 내용',
+  },
+]
+
+function reducer(state, action) {
+  return state
+}
 
 function App() {
-  const nav = useNavigate()
-
-  const onClickButton = () => {
-    nav('/new')
-  }
+  const [data, dispatch] = useReducer(reducer, [mockData])
 
   return (
     <>
-      <div>
-        <img src={getEmotionImage(1)} />
-        <img src={getEmotionImage(2)} />
-        <img src={getEmotionImage(3)} />
-        <img src={getEmotionImage(4)} />
-        <img src={getEmotionImage(5)} />
-      </div>
-      <div>
-        <Link to={'/'}>Home</Link>
-        <Link to={'/new'}>New</Link>
-        <Link to={'/diary'}>Diary</Link>
-      </div>
-      <button onClick={onClickButton}>New페이지로이동</button>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
         <Route path="/diary/:id" element={<Diary />} />
+        <Route path="/edit/:id" element={<Edit />} />
         <Route path="*" element={<Notfound />} />
       </Routes>
     </>
