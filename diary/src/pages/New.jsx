93f83ef56559c2e90efbc19ba653d928302c9e55
proud_oooof <parkjanghan2256@gmail.com>
2025-08-04 20:59:47 +0900

    feat : New 페이지 완성

--- diary/src/pages/New.jsx
+++ diary/src/pages/New.jsx
@@ -1,5 +1,39 @@
+import Header from '../components/Header'
+import Editor from '../components/Editor'
+import Button from '../components/Button'
+import { useNavigate } from 'react-router-dom'
+import { useContext } from 'react'
+import { DiaryDispatchContext } from '../App'
+
 const New = () => {
-    return <div>New</div>;
+  const nav = useNavigate()
+  const { onCreate } = useContext(DiaryDispatchContext)
+
+  const onSubmit = (input) => {
+    onCreate(input.createdDate.getTime(), input.emotionId, input.content)
+    nav('/', { replace: true })
+  }
+
+  return (
+    <div>
+      <div>
+        <Header
+          title={'새 일기 쓰기'}
+          leftChild={
+            <Button
+              text={'뒤로 가기'}
+              onClick={() => {
+                nav(-1)
+              }}
+            />
+          }
+        />
+      </div>
+      <div>
+        <Editor onSubmit={onSubmit} />
+      </div>
+    </div>
+  )
 }
 
-export default New;
+export default New
